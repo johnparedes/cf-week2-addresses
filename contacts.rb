db06421a179eb1825fa5f35efacd4a0f80f1e8c0
John Paredes <john.paredes@gmail.com>
2013-08-19 20:36:05 -0700

    Updated to add age prompt.

--- contacts.rb
+++ contacts.rb
@@ -4,6 +4,7 @@ class Person
   attr_accessor :first_name
   attr_accessor :last_name
   attr_accessor :phone
+  attr_accessor :age
 
   def full_name
     first_name + " " + last_name
@@ -18,10 +19,10 @@ class Contacts
       command = ask("> ")
 
       if command == "list"
-        puts "Name".ljust(30) + "Phone".rjust(12)
-        puts "-" * 42
+        puts "Name".ljust(30) + "Phone".ljust(20) + "Age".rjust(2)
+        puts "-" * 52
         people.each do |person|
-          puts person.full_name.ljust(30) + person.phone.rjust(12)
+          puts person.full_name.ljust(30) + person.phone.ljust(20) + person.age.rjust(2)
         end
 
       elsif command == "add"
@@ -30,6 +31,7 @@ class Contacts
         person.first_name = ask("First Name: ")
         person.last_name  = ask("Last Name: ")
         person.phone      = ask("Phone: ")
+        person.age        = ask("Age: ")
 
         people.push(person)
       elsif command == "quit"
